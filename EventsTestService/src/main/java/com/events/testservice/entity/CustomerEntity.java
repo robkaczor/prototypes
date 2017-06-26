1cd6d184407e322c232f11665eee4d6efac38296
i314961 <robert_kaczor@bcbsil.com>
2017-06-26 11:10:23 -0500

    fixed imports

--- EventsTestService/src/main/java/com/events/testservice/entity/CustomerEntity.java
+++ EventsTestService/src/main/java/com/events/testservice/entity/CustomerEntity.java
@@ -6,8 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.events.testservice.rest.v1.dto.CustomerDto.Builder;
-
 /**
  * The customer entity object.
  * @author Robert Kaczor
